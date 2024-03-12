8fa29c3e169278b399c1c3534c4fb63696d46fd6
vitorqueirosz <vitor.queiroz16@hotmail.com>
2024-03-12 20:05:09 -0300

    feat: add GetUserByApiKey query

--- internal/database/users.sql.go
+++ internal/database/users.sql.go
@@ -42,3 +42,20 @@ func (q *Queries) CreateUser(ctx context.Context, arg CreateUserParams) (User, e
 	)
 	return i, err
 }
+
+const getUserByApiKey = `-- name: GetUserByApiKey :one
+SELECT id, name, api_key, created_at, updated_at FROM users WHERE api_key = $1
+`
+
+func (q *Queries) GetUserByApiKey(ctx context.Context, apiKey string) (User, error) {
+	row := q.db.QueryRowContext(ctx, getUserByApiKey, apiKey)
+	var i User
+	err := row.Scan(
+		&i.ID,
+		&i.Name,
+		&i.ApiKey,
+		&i.CreatedAt,
+		&i.UpdatedAt,
+	)
+	return i, err
+}
--- sql/queries/users.sql
+++ sql/queries/users.sql
@@ -3,3 +3,5 @@ INSERT INTO users (id, name, api_key, created_at, updated_at)
 VALUES ($1, $2, encode(sha256(random()::text::bytea), 'hex'), $3, $4)
 RETURNING *;
 
+-- name: GetUserByApiKey :one
+SELECT * FROM users WHERE api_key = $1;
