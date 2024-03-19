ea54a4597b2ed9fc3ab757274a0a24210945d932
vitorqueirosz <vitor.queiroz16@hotmail.com>
2024-03-19 19:48:41 -0300

    feat: add UpdateCurrencyPrice and CreateCurrencies methods to updateCurrency handler

--- scraper.go
+++ scraper.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"context"
+	"database/sql"
 	"fmt"
 	"log"
 	"net/http"
@@ -9,6 +11,7 @@ import (
 	"time"
 
 	"github.com/PuerkitoBio/goquery"
+	"github.com/google/uuid"
 	"github.com/vitorqueirosz/gwallet/internal/database"
 )
 
@@ -26,7 +29,7 @@ func startCryptoScrapper(db *database.Queries) {
 		log.Fatal("Error parsing HTML:", err)
 	}
 
-	ticker := time.NewTicker(time.Duration(10) * time.Second)
+	ticker := time.NewTicker(time.Minute)
 
 	for ; ; <-ticker.C {
 		wg := &sync.WaitGroup{}
@@ -43,6 +46,45 @@ func startCryptoScrapper(db *database.Queries) {
 
 func updateCurrency(db *database.Queries, wg *sync.WaitGroup, c Currency) {
 	defer wg.Done()
+
+	currency, err := db.GetCurrencyByCode(context.Background(), c.Code)
+	if err != nil {
+		if err != sql.ErrNoRows {
+			log.Fatal("Error fetching currency by code", err)
+		}
+	}
+
+	if currency.Code == "" {
+		cc, err := db.CreateCurrencies(context.Background(), database.CreateCurrenciesParams{
+			ID:        uuid.New(),
+			Name:      c.Name,
+			Code:      c.Code,
+			Price:     c.Price,
+			CreatedAt: time.Now().UTC(),
+			UpdatedAt: time.Now().UTC(),
+		})
+		if err != nil {
+			log.Fatal("Error creating new currency", err)
+		}
+
+		fmt.Println(cc)
+		return
+	}
+
+	if c.Price == currency.Price {
+		return
+	}
+
+	db.UpdateCurrencyPrice(context.Background(), database.UpdateCurrencyPriceParams{
+		ID:    currency.ID,
+		Price: currency.Price,
+	})
+	fmt.Println(currency)
+}
+
+func formatPrice(p string) string {
+	price := strings.Replace(p, ",", "", 1)
+	return strings.Replace(price, "$", "", 1)
 }
 
 func cryptoScrapper(doc *goquery.Document) []Currency {
@@ -58,7 +100,7 @@ func cryptoScrapper(doc *goquery.Document) []Currency {
 			currencies = append(currencies, Currency{
 				Name:  currencyName,
 				Code:  currencyCode,
-				Price: strings.Replace(price, ",", "", 1),
+				Price: formatPrice(price),
 			})
 		}
 	})
